e231fb6e7dfcdb9a5e3a2739b37bf6a06eb3d54f
Benjamin Currie <bencurrieschool@gmail.com>
2022-08-19 16:34:13 -0400

    Final

--- models/dish.js
+++ models/dish.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const reviewSchema = new mongoose.Schema(
   {
     content: String,
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, //< - we want to make sure a review is always tied to a user
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     userName: String,
   },
   {
